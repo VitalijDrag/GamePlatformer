41a9824635b9a40644f6e08dfb8dd6a9b6c4bfb0
Vitalijus <vitalij.dragunovic@gmail.com>
2022-01-07 18:33:45 +0200

    Removed logging, added step counting.

--- src/main/java/com/nortal/platformer/Game.java
+++ src/main/java/com/nortal/platformer/Game.java
@@ -17,6 +17,7 @@ public class Game {
 	private Platform activePlatform;
 	private final String gameFile;
 	private List<Platform> platforms;
+	private int numberOfSteps = 0;
 
 	public Game(String gameFile) {
 		this.gameFile = gameFile;
@@ -28,38 +29,35 @@ public class Game {
 		// Start of the game at platform 0
 		activePlatform = platforms.get(0);
 		activePlatform.setVisited(true);
-		
-		Platform lastPlatform = platforms.get(platforms.size()-1);
+
+		Platform lastPlatform = platforms.get(platforms.size() - 1);
 		while (activePlatform.getIndex() != lastPlatform.getIndex()) {
 			Platform nextPlatform = findNextPlatform();
 			jumpTo(nextPlatform);
 		}
-			
-		System.out.println("Congratulations! You have rached the final platform " + lastPlatform.getIndex());
+
+		System.out.println("Congratulations! You have rached the final platform " + lastPlatform.getIndex() + " in "
+				+ numberOfSteps + " steps.");
 	}
 
 	private Platform findNextPlatform() throws HandledException {
 		Platform followingPlatform = getPlatform(activePlatform.getIndex() + 1);
-		
+
 		if (followingPlatform.isVisited()) {
-			System.out.println("Moving forward to visited platform. Getting " + followingPlatform.getCost() + " points");
 			points = points + followingPlatform.getCost();
 			return followingPlatform;
-		}
-		else if (followingPlatform.getCost() <= points) {
-			System.out.println("Moving forward to new platform. Paying " + followingPlatform.getCost() + " points");
+		} else if (followingPlatform.getCost() <= points) {
 			points = points - followingPlatform.getCost();
 			followingPlatform.setVisited(true);
 			return followingPlatform;
 		} else {
 			Platform previousPlatform = getPlatform(activePlatform.getIndex() - 1);
-			System.out.println("Moving backwards to visited platform. Getting " + previousPlatform.getCost() + " points");
 			points = points + previousPlatform.getCost();
 			return previousPlatform;
 		}
 	}
-	
-	private Platform getPlatform (int index) throws HandledException {
+
+	private Platform getPlatform(int index) throws HandledException {
 		try {
 			return platforms.get(index);
 		} catch (IndexOutOfBoundsException e) {
@@ -85,8 +83,7 @@ public class Game {
 	 * @param platform - Platform that you are going to jump to.
 	 */
 	public void jumpTo(Platform platform) {
-		System.out.println("Moving from platform (" + activePlatform.getIndex() + ", " + activePlatform.getCost() 
-		+ ") to platform (" + platform.getIndex() + ", " + platform.getCost() + "). Current points " + points);
+		numberOfSteps++;
 		activePlatform = platform;
 	}
 }
